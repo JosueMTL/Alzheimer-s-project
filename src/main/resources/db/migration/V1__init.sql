26f4c112670d0f0930a80f8384e677913f868350
Josue MTL <116099684+JosueMTL@users.noreply.github.com>
2023-12-09 13:23:43 -0500

    commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    gmail VARCHAR(255),
+    password VARCHAR(255)
+    );
+
+CREATE TABLE IF NOT EXISTS configurations (
+    id SERIAL PRIMARY KEY,
+    users_id INT,
+    volume INT,
+    alert_tones VARCHAR(255),
+    FOREIGN KEY (users_id) REFERENCES users(id)
+    );
+
+CREATE TABLE IF NOT EXISTS patienteInfo (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    date_birth DATE,
+    date_diagnosis DATE,
+    stage INT
+    );
+
+CREATE TABLE IF NOT EXISTS reminders (
+    id SERIAL PRIMARY KEY,
+    users_id INT,
+    patientInfo_id INT,
+    title VARCHAR(255),
+    description TEXT,
+    date DATE,
+    time TIME,
+    status VARCHAR (500),
+    active BOOLEAN,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (patientInfo_id) REFERENCES patienteInfo(id)
+    );
+
+CREATE TABLE IF NOT EXISTS interactions (
+    id SERIAL PRIMARY KEY,
+    patienteInfo_id INT,
+    users_message VARCHAR (200),
+    assistant_response VARCHAR (200),
+    date_time_interaction TIMESTAMP,
+    request BOOLEAN,
+    FOREIGN KEY (patienteInfo_id) REFERENCES patienteInfo(id)
+    );
+
+CREATE TABLE IF NOT EXISTS rfid_tags (
+    id SERIAL PRIMARY KEY,
+    interaction_id INT,
+    users_id INT,
+    read_date DATE,
+    location VARCHAR(255),
+    FOREIGN KEY (interaction_id) REFERENCES interactions(id),
+    FOREIGN KEY (users_id) REFERENCES users(id)
+    );
