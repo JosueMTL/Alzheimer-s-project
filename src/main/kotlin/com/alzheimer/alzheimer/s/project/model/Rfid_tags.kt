974e55cb6186051a73a74a5a915576b7c119baa6
jonatanjc <jcquezada.2@sudamericano.edu.ec>
2023-12-20 19:42:47 -0800

    commit

--- src/main/kotlin/com/alzheimer/alzheimer/s/project/model/Rfid_tags.kt
+++ src/main/kotlin/com/alzheimer/alzheimer/s/project/model/Rfid_tags.kt
@@ -1,11 +1,16 @@
 package com.alzheimer.alzheimer.s.project.model
 
-import java.sql.Date
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.Date
 
 @Entity
 @Table(name = "rfid_tags")
-class RFIDTag {
+class Rfid_tags {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
@@ -23,5 +28,3 @@ class RFIDTag {
     @Column(name = "location")
     var location: String? = null
 }
-
-
